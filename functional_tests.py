f39ce0d59217f564306dd78bbd00729c6c253ed2
Jan Van Bruggen <jancvanbruggen@gmail.com>
2015-02-25 19:52:46 -0800

    Remove players and fix graphs filename parsing

--- functional_tests.py
+++ functional_tests.py
@@ -17,16 +17,5 @@ def test_correct_number_of_seeds():
     assert len(seeds) == 1750
 
 
-def test_one_player_seeds_are_highest_degree():
-    graph_path = 'graphs/1.5.01from2501.json'
-    with open(graph_path) as graph_file:
-        graph = nx.Graph(load(graph_file))
-    sorted_degree_nodes = [node for node, _ in sorted(graph.degree_iter(), key=itemgetter(1), reverse=True)]
-    highest_degree_nodes = sorted_degree_nodes[:5] * NUM_ROUNDS
-    seeds = run(graph_path)
-    assert seeds == tuple(highest_degree_nodes)
-
-
 if __name__ == '__main__':
     test_correct_number_of_seeds()
-    test_one_player_seeds_are_highest_degree()
