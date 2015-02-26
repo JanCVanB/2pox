f7ba5bd498e85a9fb7a39ae4c689a4cb776f31ba
Jan Van Bruggen <jancvanbruggen@gmail.com>
2015-02-25 22:56:33 -0800

    Fix some things, change to int weights, write separate seeds

--- sim_run.py
+++ sim_run.py
@@ -4,7 +4,6 @@
 """
 from csv import reader
 from json import load
-from pprint import pprint
 
 import seed
 import sim
@@ -21,7 +20,7 @@ def read_config(config_path):
         next(config_reader)  # Skip weights header
         all_weights = []
         for row in config_reader:
-            all_weights.append([float(x) for x in row])
+            all_weights.append([int(x) for x in row])
     return run_number, graph_path, all_weights
 
 
@@ -30,7 +29,7 @@ def read_graph(graph_path):
         return load(graph_file)
 
 
-def read_seeds(seed_path, weights):
+def read_seeds(seed_path):
     flat_seeds = []
     with open(seed_path) as seed_file:
         seed_reader = reader(seed_file)
@@ -41,29 +40,41 @@ def read_seeds(seed_path, weights):
     return nested_seeds
 
 
-def write_seeds(run_number, graph_path, weights):
-    new_graph_path = graph_path.replace('graphs/', 'simseeds/run{}-'.format(run_number))
+def write_seeds(run_number, graph_path, weights, weights_string):
+    new_graph_path = graph_path.replace('graphs/', 'simseeds/run{}weights{}-'.format(run_number, weights_string))
     seed.write_seeds(new_graph_path, seed.run(graph_path, weights))
     return new_graph_path.replace('.json', '.seeds.txt')
 
 
 def run(config_path):
     run_number, graph_path, all_weights = read_config(config_path)
+    all_weights_strings = [','.join(str(w) for w in weights) for weights in all_weights]
     seed_paths = []
-    for weights in all_weights:
-        seed_paths.append(write_seeds(run_number, graph_path, weights))
+    for i, weights in enumerate(all_weights):
+        weights_string = all_weights_strings[i]
+        print 'calculating seeds for weights={}'.format(weights_string)
+        seed_paths.append(write_seeds(run_number, graph_path, weights, weights_string))
     graph = read_graph(graph_path)
     all_seeds = []
     for i in range(len(all_weights)):
-        all_seeds.append(read_seeds(seed_paths[i], all_weights[i]))
-    all_weights_strings = [','.join(str(w) for w in weights) for weights in all_weights]
+        all_seeds.append(read_seeds(seed_paths[i]))
     nodes = {all_weights_strings[i]: all_seeds[i] for i in range(len(all_weights))}
+    print 'running sim'
     results = sim.run(graph, nodes, NUM_ROUNDS)
+    rank_strategies = lambda score_dict: sorted(score_dict.keys(), key=lambda x: score_dict[x], reverse=True)
     for round_number, result in enumerate(results):
         print '-' * 160
         print 'Graph {}'.format(graph_path)
         print 'Round {}'.format(round_number)
-        pprint(result)
+        scores, seeds = result
+        for strategy in rank_strategies(scores):
+            print '{} won {} nodes with these seeds: {}'.format(strategy, scores[strategy], seeds[strategy])
+    print '-' * 160
+    all_scores = [result[0] for result in results]
+    avg_scores = {strategy: float(sum(scores[strategy] for scores in all_scores)) / len(all_scores)
+                  for strategy in all_scores[0]}
+    for strategy in rank_strategies(avg_scores):
+        print '{} averaged {} nodes'.format(strategy, avg_scores[strategy])
 
 
 if __name__ == '__main__':
